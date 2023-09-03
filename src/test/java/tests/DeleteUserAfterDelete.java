2e8295925f1ef55951d6127156d31057a8b6dff5
andrey3636 <111062472+andrey3636@users.noreply.github.com>
2023-09-03 20:42:13 +0300

    Update DeleteUserAfterDelete.java

--- src/test/java/tests/DeleteUserAfterDelete.java
+++ src/test/java/tests/DeleteUserAfterDelete.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// Класс тестировщик удаления пользователя
 public class DeleteUserAfterDelete extends BaseTest {
     String endpoint = "/users/";
+    
+    // Тест на удачное удаление пользователя и неудачное повторное удаление
     @Test
     public void successDelete() {
         String email = getRandomEmail();
@@ -18,11 +21,15 @@ public class DeleteUserAfterDelete extends BaseTest {
                 .generate_magic_link(false)
                 .build();
 
+        // Создание случайного пользователя
         Response response = postRequest(endpoint, requestBody);
+        // Проверка кода ответа
         assertEquals(201, response.getStatusCode());
         response = deleteRequest(endpoint + email);
+        // Проверка кода ответа
         assertEquals(200, response.getStatusCode());
         response = deleteRequest(endpoint + email);
+        // Проверка кода ответа
         assertEquals(404, response.getStatusCode());
     }
 }
